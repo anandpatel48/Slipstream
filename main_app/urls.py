d037abf7664149e81e39be29458c68056d49c53e
anandpatel48 <anandrpatel48@gmail.com>
2022-06-06 11:53:48 -0400

    bets model and log in/log out done

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name = "home"),    
+    path('', views.Home.as_view(), name = "home"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.Signup.as_view(), name="signup") 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
-from django.veiws.generic.base import TemplateView
+from django.views.generic.base import TemplateView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
 # Create your views here.
 
 class Home(TemplateView):
     template_name = "home.html"
     
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, "registration/signup.html", context)
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
+
