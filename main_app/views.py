3386da92d7716d88438ef6b4a4995ef1fc4c9fe9
anandpatel48 <anandrpatel48@gmail.com>
2022-06-06 09:24:28 -0400

    got home template up and running

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.veiws.generic.base import TemplateView
 # Create your views here.
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse('SlipStream Home Page, this will also include information about the app')
+class Home(TemplateView):
+    template_name = "home.html"
+    
