720f2867318f6268dcfa4756a77dce80b62b5b48
anandpatel48 <anandrpatel48@gmail.com>
2022-06-21 17:36:01 -0400

    profile

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,6 +4,7 @@ from .models import Bets
 from .models import Comment
 from .models import Photo
 # Register your models here.
+# Still need to work on profile
 admin.site.register(Profile)
 admin.site.register(Bets)
 admin.site.register(Comment)
